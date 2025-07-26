9f8212619de7a74f56d4b977b03a244481eab5c9
Wasgton Gomes <71408084+WasgtonGomes@users.noreply.github.com>
2025-07-25 22:42:24 -0400

    Commit operacoes basicas e graficos em R
    Script que demonstra operações aritméticas, vetores e criação de gráficos simples no R.

---
+++ PROJETO GERAL E INICIAL.R
@@ -0,0 +1,152 @@
+
+#Desenvolvimento até agora
+#--------------------------------------------------------------------------------------------
+  
+  # operações matemáticas simples
+  # Adição e subtração
+  4+2
+63-6
+
+
+
+#Multyiplicação
+#(*)
+
+4*4
+78*1
+
+
+#divisão
+# (/)
+
+4/2
+
+5/2 #divisão real
+
+8%/%5 #divisão eclisiana
+
+8%%5 #Resto da divisão
+
+# potenciação
+# ^ ou **
+
+5^2
+
+
+# Raiz quadrada
+# sqrt()
+
+sqrt(4)
+
+4^(1/2) # raiz quadrada
+27^(1/3) # raiz cubica
+
+# ordem de precredência
+
+#1º parenteses ()
+#2º potencias e raizes
+#3º multiplicação e divisão
+#4º adição e sibitração
+
+(9-6)/(4+2)*(3^3)
+
+
+
+
+(2 + 3)*(5-4)
+
+(-3)+((4*5)/2)-7
+
+((4*9)^(1/2))+(31/(81^(1/3)))
+
+(81^(1/3))
+
+# expressões algebricas
+#atribuições de variaveis e vetores
+
+x= 7
+
+y=9
+
+
+x+y
+
+#Atribuição
+
+x<- 17
+sqrt(y)
+
+# vetor
+# intervalos reais
+
+p <- -2:5
+
+w <- 5:12
+
+p *w
+
+
+# operação de vetores precisam ter o número de elementos equivalrnte ou ter o número de elemteos multiplos um do outro
+
+
+z <- c(-2, 0 , 15, 4, 7, 21 , 96 ,-9)
+
+
+
+z*2+w-3
+w
+
+
+
+
+
+
+x <- 3
+y <- -1:3
+z <- c(-2, 4, 8, 11, 3)
+ 
+
+
+
+
+x*y
+z^x
+
+
+
+# graficos do r 
+
+# graficos de disperção
+
+
+X<- 1:5
+
+plot(x)
+
+# GRAFICO DE BARRAS 
+
+barplot(z)
+
+
+# grafíco de caixa
+
+boxplot(z)
+
+
+# grafíco pizza (grafíco de setores)
+
+x<- 2:9
+
+
+pie(x)
+
+
+
+x<- -5:5
+
+y<- x^2
+
+plot(y)
+
+
+
